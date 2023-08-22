c482f95bbbacc44fb7823b8247750841ec3ad550
hingchuan <hingchuan.chan@hytechc.com>
2023-08-23 01:32:55 +0800

    add separate security config for oauth2 and api.

--- src/main/java/com/example/todolist/TodolistApplication.java
+++ src/main/java/com/example/todolist/TodolistApplication.java
@@ -2,8 +2,12 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityAutoConfiguration.class))
 public class TodolistApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/todolist/config/Oauth2Config.java
+++ src/main/java/com/example/todolist/config/Oauth2Config.java
@@ -2,6 +2,7 @@ package com.example.todolist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,20 +10,18 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@Order(1)
-//@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class }) //disable to access h2-console
-public class SecurityConfig  {
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class Oauth2Config {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain oauth2FilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/v1/**").permitAll(); // Secure API endpoints
-                //.and()
-                //.httpBasic();
+                .antMatchers("/").permitAll()
+                .and()
+                .oauth2Login();
         return http.build();
     }
-
 }
